e58b329dd013331b83d1890d6df63bf6a442d399
RandieSasongko <randie.sasongko08@mhs.mdp.ac.id>
2023-02-22 09:03:59 +0700

    update Coba Login

--- app/src/main/java/com/example/pertemuan1/MainActivity.java
+++ app/src/main/java/com/example/pertemuan1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.pertemuan1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvSesi;
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSesi = findViewById(R.id.tv_sesi);
+        btnLogout = findViewById(R.id.btn_logout);
     }
 }
